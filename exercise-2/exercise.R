935eff5038213c44049451a650f36b9e7dd3980d
Royce Abuda <roabuda@uw.edu>
2017-10-12 15:16:56 -0700

    do exercise 1,2 and 4

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,34 +1,34 @@
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100)
-
+employees <- c(paste("Employee", 1:100))
 
 # Create a vector of 2014 salaries using the runif function
-
+salaries.in.2014 <- c(runif(100,12000,100000))
 
 # Create a vector of 2015 salaries that are typically higher than the 2014 salaires (use runif again)
-
+salaries.in.2015 <- c(salaries.in.2014*runif(100,0.9,1.2))
 
 # Create a data.frame 'salaries' by combining the vectors you just made
-
+salaries <- data.frame(employees, salaries.in.2014, salaries.in.2015)
 
 # Create a column 'raise' that stores the size of the raise between 2014 and 2015
-
+salaries$raise <- salaries.in.2015 - salaries.in.2014
 
 # Create a column 'got.raise' that is TRUE if the person got a raise
-
+salaries$get.raise <- salaries$raise > 0
 
 # Retrieve values from your data frame to answer the following questions:
 
 # What was the 2015 salary of employee 57
-
+salary.of.57 <- (salaries[57,"salaries.in.2015"])
 
 # How many employees got a raise?
-
+employees.got.raise <- (length(c(salaries$get.raise)==TRUE))
 
 # What was the value of the highest raise?
-
+highest.raise <- (max(salaries$raise))
 
 # What was the name of the employee who recieved the highest raise?
-
+employee.highest.raise <- salaries[salaries$raise==highest.raise, 1]
 
 # What was the largest decrease in salaries between the two years?
 
